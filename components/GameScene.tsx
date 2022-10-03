696945811cd791509e16f6c1b73824852fd5e292
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2022-10-03 00:03:35 -0300

    play icon and style

--- components/GameScene.tsx
+++ components/GameScene.tsx
@@ -1,22 +1,20 @@
 import React from "react";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import Option from "./Option";
 
 export default function GameScene() {
   return (
     <div className="w-96 h-[70vh] bg-white p-8 rounded-3xl shadow-2xl">
       <div className="text-3xl font-semibold">O QUE VOCÊ OUVE?</div>
-      <div className="pt-16 pb-16">placeholder da resposta</div>
-      <div className="grid gap-2 items-center pt-16 pb-16">
-        <div className="border-4 py-4 px-8 hover:bg-rose-600 hover:text-white text-zinc-800 hover:shadow-2xl border-rose-600 rounded-full font-bold text-2xl transition-all">
-          2ª maior
-        </div>
-        <div className="border-4 py-4 px-8 hover:bg-rose-600 hover:text-white text-zinc-800 hover:shadow-2xl border-rose-600 rounded-full font-bold text-2xl transition-all">
-          4ª maior
-        </div>
-        <div className="border-4 py-4 px-8 hover:bg-rose-600 hover:text-white text-zinc-800 hover:shadow-2xl border-rose-600 rounded-full font-bold text-2xl transition-all">
-          5ª justa
-        </div>
+      <div className="grid pt-16 pb-8 cursor-pointer shad place-content-center">
+        <BsFillPlayCircleFill size={80} color="#e11d48" />
       </div>
-      <div className="bottom-0 relative text-right">
+      <div className="grid gap-2 items-center pt-16 pb-16">
+        <Option>2ª maior</Option>
+        <Option>3ª maior</Option>
+        <Option>8ª justa</Option>
+      </div>
+      <div className="bottom-0 relative text-right text-zinc-700">
         Capítulo 1 - Intervalos
       </div>
     </div>
---
+++ components/Option.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function Option({ children }: Props) {
+  return (
+    <div className="border-2 py-4 px-8 hover:bg-rose-600 hover:text-white text-zinc-800 hover:shadow-2xl border-rose-600 rounded-full font-bold text-2xl transition-all">
+      {children}
+    </div>
+  );
+}
